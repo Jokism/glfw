04e3727545e4632e98b47ae9742087eccff3c6bc
Jokism <Jokism@host>
2021-06-08 01:07:40 -0400

    update premake

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +21,6 @@ project "GLFW"
 
     filter "system:windows"
     systemversion "latest"
-    staticruntime "on"
 
     files
     {
@@ -48,5 +48,3 @@ project "GLFW"
     filter "configurations:Release"
         runtime "Release"
         optimize "on"
-
-    buildoptions "/MT"
